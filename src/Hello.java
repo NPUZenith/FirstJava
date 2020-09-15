07f27907eec50c18ed10e6089798ed9ec61ec92f
NPUZenith <58347499+NPUZenith@users.noreply.github.com>
2020-09-15 17:38:59 -0400

    First Java Project.

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * This file will say Hello!
+ * @author Zenith
+ * @version 9/15/20
+ */
+
+ public class Hello{
+     public static void main(String[] args) {
+        System.out.println("Hello!");
+     }
+ }
